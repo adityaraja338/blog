dfb6b0cd2d1525d6d770f4f39aa28597534c0aec
Sachin kumar gupta <sachinkg1309@gmail.com>
2025-04-20 22:50:31 +0530

    style(App): enhance layout and apply Lexend font family

--- src/App.jsx
+++ src/App.jsx
@@ -7,34 +7,40 @@ import { Footer, Header } from "./components";
 import { Outlet } from "react-router-dom";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const dispatch = useDispatch();
+	const [loading, setLoading] = useState(true);
+	const dispatch = useDispatch();
 
-  useEffect(() => {
-    authService
-      .getCurrentUser()
-      .then((userData) => {
-        if (userData) {
-          dispatch(login({ userData }));
-        } else {
-          dispatch(logout());
-        }
-      })
-      .finally(() => setLoading(false));
-  }, []);
+	useEffect(() => {
+		authService
+			.getCurrentUser()
+			.then((userData) => {
+				if (userData) {
+					dispatch(login({ userData }));
+				} else {
+					dispatch(logout());
+				}
+			})
+			.finally(() => setLoading(false));
+	}, []);
 
-  //Conditional rendering
-  return !loading ? (
-    <div className='min-h-screen w-full flex flex-wrap content-between bg-[#111]'>
-    <div className='w-full block'>
-        <Header />
-        <main>
-        <Outlet />
-        </main>
-        <Footer />
-      </div>
-    </div>
-  ) : null;
+	//Conditional rendering
+	return !loading ? (
+		<div className="relative min-h-screen w-full flex flex-wrap bg-gradient-to-t  from-[#111] via-[#030712] to-[#02071cf9]">
+			<div className="w-full block ">
+				<div className="top-0">
+					<Header />
+				</div>
+				<div className="">
+					<main>
+						<Outlet />
+					</main>
+				</div>
+				<div className="bottom-0">
+					<Footer />
+				</div>
+			</div>
+		</div>
+	) : null;
 }
 
 export default App;
